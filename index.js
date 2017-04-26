34a8cb1d839b17082bcd74b697f7bd1bf9201f06
Hoonio <hoonio@hoonio.com>
2017-04-26 13:41:46 +0100

    post endpoint for form submission

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  res.send('login success');
+});
 
 app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, './index.html'));
